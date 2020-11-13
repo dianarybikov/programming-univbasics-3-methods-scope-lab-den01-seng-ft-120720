b0a9fcb2cb9f192b415cf881aff6822d4d437680
Diana Rybikov <diana.ryb214@gmail.com>
2020-11-13 19:45:05 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,30 @@
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  status = "Thank You Mario! But Our Princess Is In Another Castle!"
+  phrase = "It's-a me, Mario!"
   puts phrase
 end
 
+mario
+
 def toadstool
-  puts status
+ status = "Thank You Mario! But Our Princess Is In Another Castle!"
+ puts status
 end
+
+toadstool
+
+def link 
+  phrase = "It's Dangerous To Go Alone! Take This."
+  puts phrase
+end
+
+link
+
+def all_phrases
+  mario = "It's-a me, Mario!"
+  toadstool = "Thank You Mario! But Our Princess Is In Another Castle! "
+  link = "It's Dangerous To Go Alone! Take This."
+puts(mario + toadstool + link)
+end
+
+all_phrases

2fe56a1a03d22773d6b19bd0b836bae75f33bc7e
Diana Rybikov <diana.ryb214@gmail.com>
2020-11-13 19:56:21 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -22,4 +22,8 @@ link
 def all_phrases
   mario = "It's-a me, Mario"
   toadstool = "Thank You Mario! But Our Princess Is In Another Castle!"
-  link = 
+  link = "It's Dangerous To Go Alone! Take This."
+  print mario, toadstool, link
+end
+
+all_phrases
